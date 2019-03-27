1ab393be063ccab15778ebde96b0703b84fd5434
Bernat <bolicubo@gmail.com>
2019-03-27 17:45:14 +0100

    added mocha and chai

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+
+describe('Test del test', function() {
+    var db = 'Nos conectamos y demas'
+
+    //Como se accede de forma asincrona
+    //a la db hay que llamar a done como callback
+    //Esto se ejecuta para resetear la db en un estado en cada test
+    beforeEach(function(done) {
+        //Devolver db al estado inicial
+        done();
+    })
+    it('Esto es una prova crack', function(done) {
+        assert.equal(1,1);
+        //Done se llama cuando termina el test
+        //A prueba de asincrono
+        done();
+    });
+    it('Esto peta', function(done) {
+        assert.equal(1,3);
+        done();
+    });
+});
